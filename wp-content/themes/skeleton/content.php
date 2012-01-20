1cb51aabc3b15543bdf291c02c59a28e6fd90252
Adan Altamira <adan.altamira@acquitygroup.com>
2012-01-20 11:57:11 -0600

    HTML markup for posts pulled into page-css-standards.php, page-html-standards.php, page-js-standards.php

---
+++ wp-content/themes/skeleton/content.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<h2 class="entry-title"><?php the_title(); ?></h2>
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+		</div><!-- .entry-content -->
+	</article><!-- #post-<?php the_ID(); ?> -->
