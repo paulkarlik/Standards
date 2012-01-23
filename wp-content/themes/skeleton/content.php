b7f1f806b4453a3841b17f13b61acdb8d538c579
Adan Altamira <adan.altamira@acquitygroup.com>
2012-01-23 08:54:36 -0600

    added links to posts and utility info

--- wp-content/themes/skeleton/content.php
+++ wp-content/themes/skeleton/content.php
@@ -10,11 +10,30 @@
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
-			<h2 class="entry-title"><?php the_title(); ?></h2>
+			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'skeleton' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
+		<div class="entry-utility">
+			<?php if ( count( get_the_category() ) ) : ?>
+				<span class="cat-links">
+					<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'skeleton' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
+				</span>
+				<span class="meta-sep">|</span>
+			<?php endif; ?>
+			<?php
+				$tags_list = get_the_tag_list( '', ', ' );
+				if ( $tags_list ):
+			?>
+				<span class="tag-links">
+					<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'skeleton' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
+				</span>
+				<span class="meta-sep">|</span>
+			<?php endif; ?>
+			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'skeleton' ), __( '1 Comment', 'skeleton' ), __( '% Comments', 'skeleton' ) ); ?></span>
+			<?php edit_post_link( __( 'Edit', 'skeleton' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
+		</div><!-- .entry-utility -->
 	</article><!-- #post-<?php the_ID(); ?> -->
